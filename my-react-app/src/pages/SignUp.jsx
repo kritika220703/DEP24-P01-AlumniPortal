d9fadc8ce966d6a3fd31aaf93a5d6e7fe1022852
Virat <2021csb1220@iitrpr.ac.in>
2024-04-07 18:16:31 +0530

    Fixes

--- my-react-app/src/pages/SignUp.jsx
+++ my-react-app/src/pages/SignUp.jsx
@@ -3,7 +3,6 @@ import StateContext from '../StateContext.js';
 import { Link, useNavigate ,useLocation} from "react-router-dom";
 import { useAuth } from '../utilities/AuthContext';
 import {auth, db} from "../firebase.js"
-import { FiCheckCircle } from 'react-icons/fi';
 import {
     createUserWithEmailAndPassword
 } from "firebase/auth";
@@ -29,7 +28,7 @@ const toastOptions = {
 
 const SignUp = () => {
     const location = useLocation();
-    const { editedUser, profilePicture } = location.state || {};
+    // const { editedUser, profilePicture } = location.state || {};
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [isValidEmail, setIsValidEmail] = useState(true);
@@ -38,17 +37,16 @@ const SignUp = () => {
     const [otp, setOtp] = useState("");
     const [isOtpSent, setIsOtpSent] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [isOtpVerified, setIsOtpVerified] = useState(false);
     const { isAdmin, setIsAdmin } = useContext(StateContext);
     // const [userRes, setUserRes] = useState(null);
-    const [showVerifyButton, setShowVerifyButton] = useState(false);
+
     const [selectedOption, setSelectedOption] = useState("");
 
     const handleOptionSelect = (e) => {
         setSelectedOption(e.target.value);
     };
 
-    console.log(editedUser);
+    // console.log(editedUser);
     const handleEmailChange = (e) => {
         const newEmail = e.target.value;
         setEmail(newEmail);
@@ -82,52 +80,52 @@ const SignUp = () => {
     
             console.log(user);
 
-            const docRef = await addDoc(collection(db, "Users"), {
-                uid: user.uid,
-                email: email, 
-                name: name,
-            });
+            // const docRef = await addDoc(collection(db, "Users"), {
+            //     uid: user.uid,
+            //     email: email, 
+            //     name: name,
+            // });
 
-            const userId = user.uid;
-            const userDocRef = doc(db, 'users', userId);
-            const colRef = collection(db, 'Users');
-            const q = query(colRef, where('uid', '==', userId));
+            // const userId = user.uid;
+            // const userDocRef = doc(db, 'users', userId);
+            // const colRef = collection(db, 'Users');
+            // const q = query(colRef, where('uid', '==', userId));
 
-            const querySnapshot = await getDocs(q);
+            // const querySnapshot = await getDocs(q);
 
-            // Check if any documents match the query
-            if (selectedOption==="Alumni" && querySnapshot.size > 0) {
-                // Get the reference to the first matching document
-                const docRef = doc(db, 'Users', querySnapshot.docs[0].id);
+            // // Check if any documents match the query
+            // if (selectedOption==="Alumni" && querySnapshot.size > 0) {
+            //     // Get the reference to the first matching document
+            //     const docRef = doc(db, 'Users', querySnapshot.docs[0].id);
             
-                if (profilePicture) {
-                    const storageRef = ref(storage,`/files/${editedUser['entryNo']}`)
-                    console.log("stor ref: ",storageRef);
-                    const uploadTask = uploadBytesResumable(storageRef, profilePicture);
+            //     if (profilePicture) {
+            //         const storageRef = ref(storage,`/files/${editedUser['entryNo']}`)
+            //         console.log("stor ref: ",storageRef);
+            //         const uploadTask = uploadBytesResumable(storageRef, profilePicture);
                 
-                    uploadTask.on(
-                        "state_changed",
-                        (err) => console.log(err),
-                        () => {
-                            console.log("innnn");
-                            getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                                console.log("URL: ",url);
-                                editedUser['profileURL']=url;
-                                // setProfileURL(process.env.PROFILE_BASE_URL + url);
-                                // setEditedUser({ ...editedUser, profileURL: url });
-                            });
-                        }
-                    ); 
-                    // console.log(profileURL);
-                }
-                editedUser['email']=email;
-                // Update the document with the new data
-                await updateDoc(docRef, editedUser);
+            //         uploadTask.on(
+            //             "state_changed",
+            //             (err) => console.log(err),
+            //             () => {
+            //                 console.log("innnn");
+            //                 getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            //                     console.log("URL: ",url);
+            //                     editedUser['profileURL']=url;
+            //                     // setProfileURL(process.env.PROFILE_BASE_URL + url);
+            //                     // setEditedUser({ ...editedUser, profileURL: url });
+            //                 });
+            //             }
+            //         ); 
+            //         // console.log(profileURL);
+            //     }
+            //     editedUser['email']=email;
+            //     // Update the document with the new data
+            //     await updateDoc(docRef, editedUser);
 
-                console.log('Document successfully updated!');
-            } else {
-            console.log('No documents found for the given query.');
-            }
+            //     console.log('Document successfully updated!');
+            // } else {
+            // console.log('No documents found for the given query.');
+            // }
 
             console.log("auth.currentuser::   ",auth.currentUser);
             login(user);
@@ -156,8 +154,10 @@ const SignUp = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if(selectedOption === "Admin"){
-
+        if(selectedOption === ""){
+            errorMessage = "Select Option Alumni/Admin.";
+            toast.error(errorMessage, toastOptions);
+            return;
         }
         
         if(email === ""){
@@ -177,13 +177,6 @@ const SignUp = () => {
             toast.error(errorMessage, toastOptions);
             return;
         }
-
-        // if (selectedOption==="Alumni" && (!editedUser || editedUser['entryNo'] === '')) {
-        //     errorMessage = "User is not a Member";
-        //     toast.error(errorMessage, toastOptions);
-        //     // navigate("/BecomeAMember");
-        //     return;
-        // }
         
         const colRef = collection(db, 'Users');
         console.log(colRef);
@@ -202,6 +195,13 @@ const SignUp = () => {
         } catch (error) {
             console.error('Error getting documents:', error);
         }
+
+        // if (selectedOption==="Alumni" && (!editedUser || editedUser['entryNo'] === '')) {
+        //     errorMessage = "User is not a Member";
+        //     toast.error(errorMessage, toastOptions);
+        //     navigate("/BecomeAMember");
+        //     return;
+        // }
 
         setIsLoading(true);
         try {
@@ -227,8 +227,6 @@ const SignUp = () => {
             } else {
                 notifySuccess("OTP sent to your email id");
                 setIsOtpSent(true);
-                setShowVerifyButton(true);
-
             }
         } catch {
             errorMessage = "Failed to create an account.";
@@ -259,7 +257,7 @@ const SignUp = () => {
             if(response.status !== 200) {
                 errorMessage = "Failed to verify OTP.";
                 toast.error(errorMessage, toastOptions);
-                navigate("/BecomeAMember");
+                navigate("/signup");
                 return;
             } else {
                 await signup(email, "666666", name);
@@ -269,11 +267,11 @@ const SignUp = () => {
                 const userId = auth.currentUser.uid;
                 // Storing user ID in local storage
                 if(selectedOption === "Alumni"){
-                    localStorage.setItem("userId", userId);
+                    // localStorage.setItem("userId", userId);
+                    localStorage.setItem("isAdmin", "false");
+                    navigate('/becomeamember', { state: { userId, email } });
                 }
                 
-                localStorage.setItem("isAdmin", "false");
-
                 if(selectedOption === "Admin"){
                     console.log("in dens admin mail frontend")
                     const data = {
@@ -295,18 +293,12 @@ const SignUp = () => {
                         navigate("/signup");
                         return;
                     }
+                    notifySuccess("Email sent to Admin for approval.")
+                    navigate("/login");
                 }
                 // if(email === "2021csb1184@iitrpr.ac.in"){
                 //     setIsAdmin(true);
                 // }
-                if(selectedOption === "Alumni"){
-                    navigate("/home");
-                }
-                else{
-                    notifySuccess("Email sent to Admin for approval.")
-                    navigate("/login");
-                }
-                
                 setIsOtpSent(false);
             }
         } catch {
@@ -318,10 +310,11 @@ const SignUp = () => {
 
   return (
     <>
-        
-        <form onSubmit={handleSubmit} className="flex justify-center items-center h-screen ">
-            <div className='flex flex-row items-center justify-center w-[950px] h-[480px] bg-white  shadow-lg rounded-2xl mt-0'>
-            <div
+        {/* <div>SignUp</div> */}
+        {!isOtpSent ? (
+            <form onSubmit={handleSubmit} className="flex justify-center items-center h-screen ">
+                <div className='flex flex-row items-center justify-center w-[950px] h-[480px] bg-white  shadow-lg rounded-2xl mt-0'>
+                <div
             className="relative bg-cover bg-center h-[480px] w-[500px] rounded-2xl flex items-center justify-center"
             style={{ backgroundImage: `url(${image})` }}
             >
@@ -380,61 +373,60 @@ const SignUp = () => {
             <p className="text-red-600">Please enter a valid email address.</p>
         )}
 
-        {showVerifyButton ? (
-              <div className='mb-6 flex flex-col items-center justify-center gap-5'>
-                <label
-                  htmlFor="otp"
-                  className="block text-gray-800 font-medium mb-2"
-                >
-                  Enter OTP:
-                </label>
-  
-                <div className="flex items-center">
-                  <input
-                    type="text"
-                    name="otp"
-                    placeholder="OTP"
-                    value={otp}
-                    onChange={(e) => setOtp(e.target.value)}
-                    className='px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 mr-2'
-                  />
-                  <button
-                    type="submit"
-                    onClick={handleOTPSubmit}
-                    className="px-4 py-2 bg-blue-800 text-white rounded-lg hover:bg-blue-900 focus:outline-none transition duration-300"
-                  >
-                    Sign Up
-                  </button>
-                </div>
-  
-                {isOtpVerified && (
-                  <div className="text-indigo-500 mt-2">
-                    <FiCheckCircle size={24} />
-                    <span className="ml-2">OTP Verified!</span>
-                  </div>
-                )}
-              </div>
-            ) : (
-              <button
-                type="button"
-                onClick={handleSubmit}
-                className="w-full px-4 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-900 focus:outline-none"
-              >
-                Generate OTP
-              </button>
-            )}
-  
-            <p className="mt-4 text-gray-600 text-center text-lg">
-              Already have an account?{" "}
-              <Link to="/LogIn" className="text-blue-800 hover:underline">
-                Log In
-              </Link>
-            </p>
+        <button
+            onClick={handleSubmit}
+            className="w-full px-4 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-900 focus:outline-none"
+        >
+            Sign Up
+        </button>
+
+        <p className="mt-4 text-gray-600 text-center text-lg">
+            Already have an account?{' '}
+            <Link to="/login" className="text-blue-800 hover:underline">
+            Log In
+            </Link>
+        </p>
                 </div>
                 </div>
             </form>
+        ):(
+            <form onSubmit={handleOTPSubmit}>
+                <div className='mb-6 flex flex-col items-center justify-center gap-5 mx-auto h-screen w-full bg-gray-200'>
+                    {/* <h1 className="text-3xl font-bold">Verify OTP</h1> */}
+                    <label
+                        htmlFor="otp"
+                        className="block text-gray-800 font-medium mb-2"
+                    >
+                        Enter OTP:
+                    </label>
+
+                    <div className="flex items-center">
+                        <input
+                            type="text"
+                            name="otp"
+                            placeholder="OTP"
+                            value={otp}
+                            onChange={(e) => setOtp(e.target.value)}
+                            className='px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 mr-2'
+                        />
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-700 focus:outline-none transition duration-300"
+                        >
+                        Verify OTP
+                        </button>
+                    </div>
+
+                    {/* <div className="text-indigo-500 mt-2">
+                        <FiCheckCircle size={24} />
+                        <span className="ml-2">OTP Verified!</span>
+                    </div> */}
+                </div>
+            </form>
+        )}
         <ToastContainer />
     </>
-)};
+  )
+}
 
 export default SignUp
