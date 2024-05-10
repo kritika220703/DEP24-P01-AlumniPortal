4762716914e6f7d9d78d6f82a04a81b2e610c203
Dhananjay Goel <dhananjay@Dhananjays-MacBook-Air.local>
2024-05-10 22:11:33 +0530

    changes

--- my-react-app/src/components/HomeProfile.jsx
+++ my-react-app/src/components/HomeProfile.jsx
@@ -7,7 +7,7 @@ import { TiNews } from "react-icons/ti";
 import { MdEventAvailable } from "react-icons/md";
 import { GiStarMedal } from "react-icons/gi";
 import { MdOutlineApproval } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import { useHistory, Switch, Route, Link } from 'react-router-dom';
 import { PiStudentFill } from "react-icons/pi";
 import { FaHandHoldingHeart } from "react-icons/fa6";
 import { FaPeopleGroup } from "react-icons/fa6";
